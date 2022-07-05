ac62cbe18004a3661a0711b51b7c8a42602a200e
vasiliySf <vasiliy.ts@gmail.com>
2022-07-05 04:31:00 +0300

    Добавьте файлы проекта.

---
+++ IAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17._6._4
+{
+    public interface IAccount
+    {
+        
+        public void CalculateInterest()
+        {
+
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp17._6._4;
+
+class programm
+{
+    public static void Main(String[] args)
+    {
+        
+        Calculator calculator = new Calculator(new BaseAccount(500.0));        
+        calculator.CalculateInterest();
+      
+        Console.WriteLine();
+                
+        Calculator calculator1 = new Calculator(new SalaryAccount(1500.0));
+        calculator1.CalculateInterest();
+               
+    } 
+
+}
+
+
